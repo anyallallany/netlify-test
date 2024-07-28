8d0237b8f1d84042c043bf83d785f770d904bc6c
Narman S <91620632+anyallallany@users.noreply.github.com>
2024-07-28 11:09:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,10 @@ const express = require("express")
 const path = require("path")
 
 const app = express()
-app.set("views", path.join(__dirname, "views"))
-app.set("view engine", "ejs")
+
 
 app.get("/", (req, res) => {
-    res.render("index", {
-        data: JSON.stringify(process.env, null, 4)
-    })
+     res.send(process.env)
 })
 
 module.exports = app
